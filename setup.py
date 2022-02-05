bb2a0172577f24b2ebeda92e820b4cdb219f7dd2
Nguyen Van Duc <duc.nv@timo.vn>
2022-02-05 15:26:16 +0700

    chore: Update setup file

--- setup.py
+++ setup.py
@@ -1,58 +1,45 @@
-#!/usr/bin/env python
 from setuptools import find_namespace_packages, setup
 import os
+import ast
 import re
 
 
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "README.md")) as f:
-    long_description = f.read()
+project_name = "dbt-athena"
+cur_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-package_name = "dbt-athena"
+def read_version():
+    _version_re = re.compile(r"version\s+=\s+(.*)")
+    _version_path = os.path.join(cur_dir, "dbt", "adapters", "athena", "__version__.py")
+    with open(_version_path, "rb") as f:
+        version = str(ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1)))
+
+    return version
 
 
-# get this from a separate file
-def _dbt_athena_version() -> str:
-    _version_path = os.path.join(
-        this_directory, "dbt", "adapters", "athena", "__version__.py"
-    )
-    _version_pattern = r"version\s*=\s*[\"'](.+)[\"']"
-    with open(_version_path) as f:
-        match = re.search(_version_pattern, f.read().strip())
-        if match is None:
-            raise ValueError(f"invalid version at {_version_path}")
-        return match.group(1)
+def read_requirements():
+    with open(os.path.join(cur_dir, "requirements.txt")) as f:
+        content = f.read()
+        requirements = content.split("\n")
+    return requirements
 
 
-package_version = _dbt_athena_version()
-description = """The athena adapter plugin for dbt (data build tool)"""
-
-
-dbt_version = "1.0"
-
-if not package_version.startswith(dbt_version):
-    raise ValueError(
-        f"Invalid setup.py: package_version={package_version} must start with "
-        f"dbt_version={dbt_version}"
-    )
+def read_readme():
+    with open(os.path.join(cur_dir, "README.md")) as f:
+        long_description = f.read()
+    return long_description
 
 
 setup(
-    name=package_name,
-    version=package_version,
-    description=description,
-    long_description=long_description,
+    name=project_name,
+    version=read_version(),
+    description="The athena adapter plugin for dbt (data build tool)",
+    long_description=read_readme(),
     long_description_content_type="text/markdown",
-    author="Thomas Elvey",
-    author_email="tomelvey@googlemail.com",
-    url="https://github.com/Tomme/dbt-athena",
+    author="Duc Nguyen",
+    author_email="vanducng.dev@gmail.com",
+    url="https://github.com/vdn-tools/dbt-athena",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
-    install_requires=[
-        "dbt-core~=1.0.0",
-        "pyathena==2.2.0",
-        "boto3==1.18.12",
-        "tenacity==6.3.1",
-    ],
+    install_requires=read_requirements(),
 )
